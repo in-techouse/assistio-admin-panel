9649608ceaed2ab7433069e9871a05ed4b2741dd
Areej <areejikram3@gmail.com>
2020-01-05 13:19:36 +0500

    Layout final

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('pages/index');
+  res.render('pages/login');
 });
 
 module.exports = router;
