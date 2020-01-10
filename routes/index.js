17d44f45e89431cce14a3abe4aac8edbb4a61c8f
Areej <areejikram3@gmail.com>
2020-01-10 12:49:37 +0500

    working on login page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('pages/login');
+  res.render('pages/login', {error: ""});
+});
+
+
+router.post('/login', function(req, res) {
+ res.json("1")
 });
 
 module.exports = router;
