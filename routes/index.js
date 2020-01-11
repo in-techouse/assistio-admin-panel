97137cff8c4435273396fa88ebed458ff68cb462
Eza Waheed <ezzaawaheed@gmail.com>
2020-01-11 11:25:52 +0500

    Working on login

--- routes/index.js
+++ routes/index.js
@@ -2,24 +2,24 @@ var express = require('express');
 var router = express.Router();
 var firebase = require('firebase');
 const firebaseConfig = {
-  apiKey: "AIzaSyApoZcuKH_IZnm-8k9S-GC9NPTazSt8ATA",
-  authDomain: "asistio-14701.firebaseapp.com",
-  databaseURL: "https://asistio-14701.firebaseio.com",
-  projectId: "asistio-14701",
-  storageBucket: "asistio-14701.appspot.com",
-  messagingSenderId: "82396410440",
-  appId: "1:82396410440:web:354a81aa37b1194bb35e7d",
-  measurementId: "G-9ZBGTZQQ02"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECCT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 };
 firebase.initializeApp(firebaseConfig);
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('pages/login', {error: ""});
+router.get('/', function (req, res) {
+  res.render('pages/login', { error: "" });
 });
 
 
-router.post('/login', function(req, res) {
- res.json("1")
+router.post('/login', function (req, res) {
+  res.json("1")
 });
 
 module.exports = router;
