0c30b8060cfb957adec9646418899ad1d44baead
Areej <areejikram3@gmail.com>
2020-01-17 12:56:35 +0500

    working on navbar

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,13 @@ var router = express.Router();
 var firebase = require('firebase');
 
 router.get('/',function(req , res){
-    res.json("Hello Admin")
+    res.render("pages/index");
+});
+router.get('/all_users',function(req , res){
+    res.render("pages/all_users");
+});
+router.get('/user',function(req , res){
+    res.render("pages/user");
 });
 
 
